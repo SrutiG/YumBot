f47b49392838a3fb0ab84a5710e73a8d649d1dbd
SrutiG <sruti.guhathakurta@gmail.com>
2019-04-06 20:34:51 -0400

    initial spoonacular API implementation

---
+++ app/spoonacular_api.py
@@ -0,0 +1,70 @@
+import requests
+import os
+
+api_url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+api_host = "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+api_key = os.environ["SPOONACULAR_API_KEY"]
+require_instructions = "&instructionsRequired=1"
+headers = {"X-RapidAPI-Host":api_host,"X-RapidAPI-key":api_key}
+
+def find_random_recipes(numRecipes):
+    new_recipes = []
+    url = api_url + "/recipes/random?number=" + str(numRecipes) + require_instructions
+    try:
+        response = requests.get(url, headers=headers)
+        for recipe in response.json()["recipes"]:
+            new_recipes.append(format_spoonacular_recipe(recipe))
+    except Exception as e:
+        print e
+        print "[Error] error getting recipe data"
+    return new_recipes
+
+def search_recipes_by_ingredient(ingredient_list):
+    new_recipes = []
+    num_recipes = "8"
+    ingredients = ",".join(ingredient_list)
+    url = api_url + "/recipes/findByIngredients?number=" \
+          + num_recipes + "&ingredients=" + ingredients
+    try:
+        response = requests.get(url, headers=headers)
+        for recipe in response.json():
+            recipe_information = get_recipe_information(recipe["id"])
+            new_recipe = format_spoonacular_recipe(recipe_information)
+            new_recipe["used_ingredient_count"] = recipe["usedIngredientCount"]
+            new_recipes.append(new_recipe)
+    except Exception as e:
+        print e
+        print "[Error] error getting recipe data when searching by ingredient"
+    return new_recipes
+
+def get_recipe_information(id):
+    url = api_url + "/recipes/" + str(id) + "/information"
+    recipe = ""
+    try:
+        response = requests.get(url, headers=headers)
+        recipe = response.json()
+    except Exception as e:
+        print e
+        print "[Error] error getting recipe information"
+    return recipe
+
+def format_spoonacular_recipe(recipe):
+    new_recipe = {"ingredients":[], "steps":[]}
+    new_recipe["name"] = recipe["title"]
+    new_recipe["id"] = recipe["id"]
+    if "ready_in_minutes" in recipe:
+        new_recipe["prep_time"] = recipe["ready_in_minutes"]
+    if "image" in recipe:
+        new_recipe["image"] = recipe["image"]
+    for ing in recipe["extendedIngredients"]:
+        new_recipe["ingredients"].append(ing["originalString"])
+    if "analyzedInstructions" in recipe:
+        for step in recipe["analyzedInstructions"][0]["steps"]:
+            new_recipe["steps"].append(step["step"])
+    else:
+        new_recipe["steps"] = recipe["instructions"].split("\n")
+    print new_recipe
+    return new_recipe
+
+
+
--- app/views.py
+++ app/views.py
@@ -1,9 +1,45 @@
 from app import app
 from flask import render_template, redirect, session, request, jsonify, url_for
+from spoonacular_api import *
+
+
 
 '''
 Default route
 '''
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if not session.get("search"):
+        session["search"] = []
+    if not session.get("recipes"):
+        session["recipes"] = find_random_recipes(4)
+        session["random_search"] = True
+    return render_template('index.html',
+                           recipes=session.get("recipes"),
+                           search=session.get("search"),
+                           random_search=session.get("random_search"))
+
+@app.route('/search/<search_value>')
+def search(search_value=None):
+    search = session.get("search")
+    if search_value not in search:
+        search.append(search_value)
+    session["search"] = search
+    recipes = search_recipes_by_ingredient(search)
+    session["recipes"] = recipes
+    if len(recipes) > 0:
+        session["random_search"] = False
+    return redirect(url_for("index"))
+
+@app.route('/remove_search/<search_value>')
+def remove_search(search_value):
+    search = session.get("search")
+    if search_value in search:
+        search.remove(search_value)
+    session["search"] = search
+    recipes = search_recipes_by_ingredient(search)
+    session["recipes"] = recipes
+    if len(recipes) > 0:
+        session["random_search"] = False
+    return redirect(url_for("index"))
+
